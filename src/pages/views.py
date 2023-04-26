b3b26ef5c357055adc1e0b53cda72c4a81013377
saichandanal <saichandana2021@gmail.com>
2023-04-26 23:01:08 +0530

    adding request in view

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kargs):
+def home_view(request ,*args, **kargs):
     return HttpResponse("<H1>Hello world</H1>")
+
+def contact_view(request, *args, **kargs):
+    return HttpResponse("<H1>Contact Details</H1>")
