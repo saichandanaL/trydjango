1400c250db58d7cdbe10aa7ef50de1eda8935c0c
saichandanal <saichandana2021@gmail.com>
2023-04-26 23:04:22 +0530

    added print statemnt to see what request , args and kargs are returning

--- src/pages/views.py
+++ src/pages/views.py
@@ -6,4 +6,6 @@ def home_view(request ,*args, **kargs):
     return HttpResponse("<H1>Hello world</H1>")
 
 def contact_view(request, *args, **kargs):
+    print(args , kargs)
+    print(request.user)
     return HttpResponse("<H1>Contact Details</H1>")
