bdc316ba167796f5e289a4682d39a608e1391355
saichandanal <saichandana2021@gmail.com>
2023-04-26 22:34:11 +0530

    added view to pages and added path to url

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home_view(*args, **kargs):
+    return HttpResponse("<H1>Hello world</H1>")
