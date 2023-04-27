4a5272cd4e7603323b3b31ea63951ae0d1142ce6
saichandanal <saichandana2021@gmail.com>
2023-04-27 23:42:46 +0530

    for loop in template

--- src/pages/views.py
+++ src/pages/views.py
@@ -12,6 +12,7 @@ def contact_view(request, *args, **kargs):
     send_context = {
         "address" : "abc",
         "phone" : "383897349",
-        "year" : [2020, 2021, 2022]
+        "year" : [2020, 2021, 2022],
+        "weather" : ["rain","sunny","wind","cold"]
     }
     return render(request, "about.html", send_context)
