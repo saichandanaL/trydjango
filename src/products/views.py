aca0bada9a8eb9ba1307c6f40e160b1f8647e19b
saichandanal <saichandana2021@gmail.com>
2023-04-28 19:50:43 +0530

    Render Data from the Database with a model by passing object directly

--- src/products/views.py
+++ src/products/views.py
@@ -5,12 +5,16 @@ from .models import Product
 
 # Create your views here.
 # obj = Product.objects.get(id = 1)
-obj = Product.objects.get( id = 6 )
+obj = Product.objects.get( id = 1 )
 
 def product_detail_view(request):
 
+    # context_obj = {
+    #     "title" : obj.title,
+    #     "description" : obj.description
+    # }
+
     context_obj = {
-        "title" : obj.title,
-        "description" : obj.description
+        "object" : obj
     }
     return render(request, "product/detail.html", context_obj)
