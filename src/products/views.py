48c0151bc62335d31f14bb6cbacb8e3198304d42
saichandanal <saichandana2021@gmail.com>
2023-04-28 19:43:46 +0530

    Render Data from the Database with a model

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+# importing model 
+from .models import Product
+
 # Create your views here.
+# obj = Product.objects.get(id = 1)
+obj = Product.objects.get( id = 6 )
+
+def product_detail_view(request):
+
+    context_obj = {
+        "title" : obj.title,
+        "description" : obj.description
+    }
+    return render(request, "product/detail.html", context_obj)
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view
+from products.views import product_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
-    path('contact/', contact_view, name="contact")
+    path('contact/', contact_view, name="contact"),
+    path('product/', product_detail_view, name="products")
 ]
