a92a690c62b6c34a5fc116ba2d8411d87378df25
saichandanal <saichandana2021@gmail.com>
2023-05-06 18:23:44 +0530

    nothing

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Prompt the user to enter the commit message
+echo "Enter commit message:"
+read commit_message
+
+# Add and commit the changes with the specified message
 git add .
-git commit -m "Dynamic url routing"
+git commit -m "$commit_message"
+
+# Push the changes to the remote repository
 git push
